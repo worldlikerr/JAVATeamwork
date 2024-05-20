52a13c5ca5e675802a66b76eb3730c79b7c655da
helloworld114514 <1423201097@qq.com>
2024-05-20 16:24:15 +0800

    test

---
+++ src/work/Test.java
@@ -0,0 +1,20 @@
+package work;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Test extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane pane= FXMLLoader.load(getClass().getResource("view/TestScene.fxml"));
+        Scene scene=new Scene(pane,500,500);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
