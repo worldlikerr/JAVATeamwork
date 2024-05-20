8a04270b56933104823b827142837f368ebb6449
helloworld114514 <1423201097@qq.com>
2024-05-20 16:27:41 +0800

    test

--- src/work/Test.java
+++ src/work/Test.java
@@ -1,9 +1,6 @@
 package work;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Test extends Application {
